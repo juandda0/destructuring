eec8ce7f6fe853d6f821ccc05d34b9e5966824ff
juandda0 <juandiegodiazave@gmail.com>
2025-04-08 17:17:11 -0500

    added features of destructuring with objects

--- destructuring-obj.js
+++ destructuring-obj.js
@@ -5,13 +5,15 @@
 const person = {
 
     name: "Juan",
+    secondName: "Diaz",
     age: 19,
 };
 
 const tname = person.name
 const tage = person.age
+const tsecondName = person.secondName
 
-console.log(tname, tage)
+console.log(tname, tage, tsecondName)
 //output: Juan, 19
 
 //destructuring way:
@@ -19,7 +21,18 @@ console.log(tname, tage)
 here, you need to use "{}" to defind the variables of the object
 and also you have to use the same name of the variable of the object
 */
-const {name, age} = person;
+const {name, age, secondName} = person;
 
-console.log(name, age)
+console.log(name, age, secondName)
+//output: Juan, 19, Diaz
+
+//also we can use another name of variable like this:
+const {name: nameJuan, age: ageJuan, secondName: secondNameJuan} = person
+console.log(nameJuan, ageJuan)
 //output: Juan, 19
+
+//and if you want to only create 1 variable, and the ress of variable you want to save in a sub-obj you can do this:
+const {name: nameJuan2, ...ressPerson} = person
+
+console.log(nameJuan2, ressPerson)
+//output: Juan { secondName: 'Diaz', age: 19 }
